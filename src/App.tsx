90e46679c00f580a0033344789524940935bbbfd
Tamara Zuk <tamarazuk@gmail.com>
2023-06-25 16:11:38 -0400

    Update template to include a sample showing how to connect to Mailjet

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,55 @@
 import HelpScout, { NOTIFICATION_TYPES } from "@helpscout/javascript-sdk";
 import { Button, DefaultStyle, Heading } from "@helpscout/ui-kit";
 import { useEffect, useState } from "react";
+import * as Mailjet from "node-mailjet";
 
 function App() {
-  const [userEmail, setUserEmail] = useState<string | undefined>(
-    "unknown user"
-  );
+  const [userEmail, setUserEmail] = useState<string | undefined>("");
+  const [userName, setUserName] = useState<string | undefined>("");
 
   useEffect(() => {
-    HelpScout.getApplicationContext().then(({ user }) =>
-      setUserEmail(user?.email)
-    );
+    HelpScout.getApplicationContext().then(({ user }) => {
+      setUserEmail(user?.email);
+      setUserName(user?.firstName + " " + user?.lastName);
+    });
   }, []);
 
-  function onClick() {
-    HelpScout.showNotification(
-      NOTIFICATION_TYPES.SUCCESS,
-      "Hello from the sidebar app"
-    );
+  function subscribeCustomer() {
+    const mailjet = new Mailjet.Client({
+      apiKey: process.env.MJ_APIKEY_PUBLIC,
+      apiSecret: process.env.MJ_APIKEY_PRIVATE,
+    });
+    mailjet
+      .post("contact", { version: "v3" })
+      .request({
+        IsExcludedFromCampaigns: false,
+        Name: userName,
+        Email: userEmail,
+      })
+      .then((result) => {
+        console.log(result.body);
+
+        HelpScout.showNotification(
+          NOTIFICATION_TYPES.SUCCESS,
+          "Contact successfully added mailing list!"
+        );
+      })
+      .catch((err) => {
+        console.log(err.statusCode);
+      });
   }
 
   return (
     <div className="App">
       <DefaultStyle />
-      <Heading level="h1">Hi, {userEmail}</Heading>
-      <br />
-      <Button size="sm" onClick={onClick}>
-        Click me
+      Hello?
+      <Button
+        size="sm"
+        onClick={() => {
+          subscribeCustomer();
+        }}
+      >
+        Add to mailing list
       </Button>
     </div>
   );
